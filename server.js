dac76b20c9f4bb86e94ac40794abefd8cf4b6aa3
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-03 17:16:45 +0100

    add tasks

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const indexRoute = require('./routes/index');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const indexRoute = require('./routes/index');
 
