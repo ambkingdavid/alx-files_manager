7b7cda8fa9b9256591be7fa4215e28191e30ac97
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-03 17:31:27 +0100

    add task

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-// const indexRoute = require('./routes/index');
+const indexRoute = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-const indexRoute = require('./routes/index');
+app.use(express.json());
 
 // Use the router as middleware
 app.use('/', indexRoute);
