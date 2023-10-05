70e6a797cc88ed250974d45973ab3caf5412bc1b
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-05 12:26:00 +0100

    add changes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -6,33 +6,32 @@ class UsersController {
     const { email, password } = request.body;
 
     if (!email) {
-      return response.status(400).send({error: 'Missing email'});
+      return response.status(400).send({ error: 'Missing email' });
     }
     if (!password) {
-      return response.status(400).send({error: 'Missing password'});
+      return response.status(400).send({ error: 'Missing password' });
     }
 
     const existingUser = await dbClient.client.db().collection('users').findOne({ email });
 
     if (existingUser) {
-      return response.status(400).send({error: 'Already exist'});
-    } else {
-      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-      const newUser = {
-        email,
-        password: hashedPassword,
-      };
-
-      // Insert the new user into the collection
-      const result = await dbClient.client.db().collection('users').insertOne(newUser);
-
-      const insertedUser = {
-        _id: result.insertedId,
-        email: newUser.email,
-      };
-      return response.status(201).send(insertedUser);
+      return response.status(400).send({ error: 'Already exist' });
     }
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    // Insert the new user into the collection
+    const result = await dbClient.client.db().collection('users').insertOne(newUser);
+
+    const insertedUser = {
+      _id: result.insertedId,
+      email: newUser.email,
+    };
+    return response.status(201).send(insertedUser);
   }
 }
 
