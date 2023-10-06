a9af4270d3fe64cf2573f1ca46ba672f4c05e93f
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-06 19:22:25 +0100

    add changes

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect)
+router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 
 router.post('/users', UsersController.postNew);
